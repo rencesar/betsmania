1e6c037d6a17b7e8efc96d60a92c5a222d056c94
Renato César <ren_cesar@outlook.com>
2017-03-30 00:44:20 -0300

    Configure SQL Heroku

--- main/settings/base.py
+++ main/settings/base.py
@@ -79,6 +79,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
