c08a73d8ffe56af6c95eeafe9854655e3f01321a
Renato César <ren_cesar@outlook.com>
2017-04-01 04:17:52 -0300

    Adicionando nome do banco

--- main/settings/test.py
+++ main/settings/test.py
@@ -1,6 +1,7 @@
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'python-test-app',
          'USER': 'postgres',
          'HOST': 'postgres',
          'PORT': 5432,
