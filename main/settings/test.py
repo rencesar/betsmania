796f42e0c6f58109dec015244cd392fa5e29cfff
Renato César <ren_cesar@outlook.com>
2017-04-03 01:16:46 -0300

    Configuring tests.py

--- main/settings/test.py
+++ main/settings/test.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'test',
+        'HOST': 'postgres',
         'PORT': '',
     }
 }
