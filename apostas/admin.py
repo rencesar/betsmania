025b43ddac3443d163f31aec101783759cd45942
Renato César <ren_cesar@outlook.com>
2017-04-24 20:33:24 -0300

    Adicionando FormView e form para tratar as pendencias

--- apostas/admin.py
+++ apostas/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from . import models
-# Register your models here.
+
+
 @admin.register(models.Bet)
 class BetAdmin(admin.ModelAdmin):
     list_display = ('user', 'code', 'match', 'value', 'type')
